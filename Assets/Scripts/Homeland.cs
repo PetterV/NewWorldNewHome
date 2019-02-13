686692522e29a6afb75618f3d1cb21869880c6f2
Petter Vilberg <petter.vilberg@gmail.com>
2019-02-13 22:22:29 +0100

    Music and Monoliths
    Music plays
    In addition, the concept of a "Homeland" exists, and Monoliths know how far away the Homeland is

---
+++ Assets/Scripts/Homeland.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Homeland : MonoBehaviour
+{
+    public Color closeColor;
+    public Color defaultColor;
+
+    Collider2D playerCollider;
+
+    public bool isWithinRange = false;
+
+    GameController gameController;
+    void Start()
+    {
+        gameController = GameObject.Find("GameController").GetComponent<GameController>();
+        playerCollider = GameObject.Find("ResourceCollider").GetComponent<Collider2D>();
+        GetComponent<SpriteRenderer>().color = defaultColor;
+    }
+
+    void Update()
+    {
+        if (GetComponent<Collider2D>().bounds.Intersects(playerCollider.bounds) && GetComponent<SpriteRenderer>().color != closeColor)
+        {
+            GetComponent<SpriteRenderer>().color = closeColor;
+        }
+        else if (GetComponent<SpriteRenderer>().color == closeColor &! GetComponent<Collider2D>().bounds.Intersects(playerCollider.bounds))
+        {
+            GetComponent<SpriteRenderer>().color = defaultColor;
+        }
+    }
+}
---
+++ Assets/Scripts/MonolithTile.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonolithTile : MonoBehaviour
+{
+    public Color closeColor;
+    public Color defaultColor;
+
+    Collider2D playerCollider;
+
+    public bool isWithinRange = false;
+    public float distanceToHomeland;
+
+    GameController gameController;
+    void Start()
+    {
+        gameController = GameObject.Find("GameController").GetComponent<GameController>();
+        playerCollider = GameObject.Find("ResourceCollider").GetComponent<Collider2D>();
+        GetComponent<SpriteRenderer>().color = defaultColor;
+    }
+
+    void Awake()
+    {
+        Transform homeland = GameObject.Find("Homeland").GetComponent<Transform>();
+        distanceToHomeland = Vector3.Distance(transform.position, homeland.position);
+    }
+
+    void Update()
+    {
+        if (GetComponent<Collider2D>().bounds.Intersects(playerCollider.bounds) && GetComponent<SpriteRenderer>().color != closeColor)
+        {
+            GetComponent<SpriteRenderer>().color = closeColor;
+        }
+        else if (GetComponent<SpriteRenderer>().color == closeColor &! GetComponent<Collider2D>().bounds.Intersects(playerCollider.bounds))
+        {
+            GetComponent<SpriteRenderer>().color = defaultColor;
+        }
+    }
+}
